cf220f7608e76351e2ac5b920a1ffb2d895b0458
Moriz82 <72836413+Moriz82@users.noreply.github.com>
2022-08-30 12:50:07 -0500

    Initial Commit

---
+++ Simon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simon
+//
+//  Created by William Dedominico on 8/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
